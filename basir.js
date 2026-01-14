e703ee136d3f819f07e52730605bb7c550b8537a
shekh basir <shekhbasir9821273349@gmail.com>
2026-01-14 20:48:59 +0530

    first commit

---
+++ basir.js
@@ -0,0 +1,222 @@
+// // // //now i am going to wrting the code for the js very eassy 
+
+// // // // const name={
+// // // //   name1:"sheikh basir",
+// // // //   name2:"sheikh rustom"
+// // // // }
+
+// // // // // console.log(name.name1,name.name2);
+// // // // const arr=["komal",kom={
+// // // // "age":"justt young ",
+// // // // }]
+
+// // // // console.log(arr[1].age)
+
+// // // //now from here i am going to working with the simple task 
+
+// // // const sabdata={
+// // //  "industry":{
+// // //   "sabcomp":["Bharatbenz","tata","makwanpur","jiwanbima","loan sat"]
+// // //  },
+// // //  "salary":[20000,30000,40000,50000,60000],
+// // //  "country":{
+// // //   "Nepal":{"batch":["bhc","b21","lal jan"],
+// // //     "role":["spl","local","kumau"]
+// // //   }
+// // //  }
+// // // }
+
+// // // // 
+// // // //now i want to print the all data 
+// // // console.log("before insertion of the data");
+// // // sabdata.industry.sabcomp.map((data,index)=>{
+// // //   console.log(data)
+// // // })
+
+// // // sabdata.industry.sabcomp.push("golxa group");
+
+
+// // // console.log("after the insertion of the data ");
+// // // sabdata.industry.sabcomp.map((data,index)=>{
+// // //   console.log(data)
+// // // })
+
+// // // sabdata.country.Nepal.role.map((data)=>{
+// // //   console.log(data)
+// // // })
+
+// // //now i am going to writng the code fr the for 
+
+// // const arr1=[1,2,3,4,5,6,7,8,9];
+// // const len=arr1.length;
+// // for(let i=0;i<len;i++){
+// //   console.log(arr1[i])
+// // }
+
+// // //logic with all types 
+// // ///by this way the formal secondry way of the normal accountancy thince ni tha 
+// // 
+// //now i am going to wrting the code for hte normal senapa
+
+
+// // const age=12;
+// // if(age>23){
+// //   console.log("i am your father ");
+// // }else{
+// //   console.log("no i am your uncle");
+// // }
+
+// //ok so let the normal step in the congresss of the pas t 
+
+
+// // if(age>1){  
+// //   console.log("this is the greater than the 1");
+ 
+// // }else if(age==12){
+// //   console.log("i am the exact the numeber 12");
+// // }else if(age >12){
+// //   console.log("this is the greater than the 12");
+// // }else{
+// //   console.log("this is the normal task")
+// // }
+
+
+// // const day=2;
+// // const day=9;
+// // switch(day){
+// //   case 1:console.log("tihis is the sunday");
+// //   break;
+// //   case 2:console.log("this is the monday");
+// //   break;
+// //   case 3:console.log("this si the tuesday");break
+// //   case 4:console.log("this is the wednesday ");break
+// //   default:console.log("i am the default day as today ")
+// // }
+
+// // const arr=[1,2,3,4,5,6,7,8,9];
+// // for(let value of arr){
+// //   console.log(value)
+// // }
+
+// //arrow function kahe ni san 
+// // const hamarfnction=()=>{
+
+// // }
+
+// //const justf
+// // const justfun=((num,num2)=>{
+// // console.log("this is the call back function");
+// // console.log(num,num2);
+// // })
+// // const ham=justfun(786,789);
+
+
+// // const hamarfun=(()=>{
+
+// // })
+// // const hamarkam=()=>{
+// //   console.log("i am the simple chij")
+// // }
+// // hamarkam()
+
+// //now i am going to working with the diff
+// // const add=(a,b)=>{
+// //   return a+b;
+
+// // }
+// // const sabdata=add(20,30);
+// // console.log(sabdata)
+// // console.log("i am your father");
+
+
+// function add(){
+//   console.log("i am your father")
+// }
+
+// add()
+// add()
+// add()
+// add()
+// add()
+// add()
+// add()
+
+
+// //see i am going to working with the noormal jobs
+// function add(...sadata){
+//   console.log(sadata);
+// }
+// const arr1=[];
+// arr1.push=add(1,2,3,4,5,6,7);
+// console.log(arr1);
+
+//means i am not going to work with the simple task we are going to work 
+
+// const hamafun=(...arr)=>{
+//   console.log(arr);
+//   let sum=0;
+//   for(let i of arr){
+//     sum+=i;
+    
+//   }
+//   return sum;
+
+// }
+// const sabdata=hamafun(12,23,34,35,46,55,)
+// console.log("the sum  of the all element ",sabdata);
+
+
+//now i am going to use the ternury operatore
+
+// const age=80
+// const result =age?"i am the oldest one":"i am the young people from the nepal";
+// console.log(result)
+
+//dynamic css m v ham dal saktani ham apan task k 
+
+
+
+// const arr=[12,23,43,45,5,6,67,87,89];
+// arr.forEach((val,index)=>{
+ 
+//   console.log(val);
+//now i a going to working the simple task
+// })
+
+// arr.pop(arr.length);
+// console.log(arr)
+
+//oposite k rup me kaam karela and the task will perform the simple manner \\\
+
+//let see i am going to wrting the code where i am taking the element by user dynamic
+
+// let arr=[];
+
+// 
+
+// const jam=prompt("enter the element ");
+// console.log(jam);
+
+//now i am going to creating the new object 
+// let hamarobj=new hamarobjobj();
+// hamarobj.name="sheikh basir";
+// console.log(hamarobj);
+//this is the simple code for the normal value it will perform 
+
+
+// const hamarobj=new Object();
+// hamarobj.name="Sheikh Basir";
+// hamarobj.age="this is thhe normal chij"
+// console.log(hamarobj);
+
+
+//by this way i am also going too creating the obj 
+
+
+function hamafun(name,age,group){
+  this.name=name;
+  this.age=age;
+  this.group=group;
+}
+const hamarobj=new hamafun("sheikh basir",34,"A+");
+console.log(hamarobj);
